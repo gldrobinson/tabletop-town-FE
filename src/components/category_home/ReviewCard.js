4bde069f7d451586b929ad2c35541ce5627b853e
gldrobinson <gldrobinson@hotmail.co.uk>
2021-11-22 16:57:12 +0000

    Sets up ReviewCard structure for each review

--- src/components/category_home/ReviewCard.js
+++ src/components/category_home/ReviewCard.js
@@ -1,7 +1,24 @@
-const ReviewCard = () => {
+const ReviewCard = ({ review }) => {
+  const {
+    title,
+    review_id,
+    designer,
+    review_image_url,
+    votes,
+    owner,
+    comment_count,
+    review_created_at,
+  } = review;
+
   return (
     <section className="ReviewCard">
-      <h4>Review cards...</h4>
+      <h3>{designer}</h3>
+      <img src={review_image_url} alt={designer} />
+      <h4>{title}</h4>
+      <h5>{owner}</h5>
+      <p>{review_created_at}</p>
+      <p>{votes}</p>
+      <p>{comment_count}</p>
     </section>
   );
 };
