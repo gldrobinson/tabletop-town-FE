5828c6c4331f9089c1acf8251e0a32f44329f20c
gldrobinson <gldrobinson@hotmail.co.uk>
2021-11-24 14:42:05 +0000

    Adds some css styling to CommentCard through review_home.css

--- src/components/review_home/CommentCard.js
+++ src/components/review_home/CommentCard.js
@@ -1,14 +1,30 @@
+import moment from "moment";
+
 const CommentCard = ({ comment }) => {
-  console.log(comment);
   const { author, comment_body, comment_created_at, comment_id, votes } =
     comment;
+  //const date = comment_created_at.split("T")[0];
 
+  const date = new Date(comment_created_at);
+  const timeAgo = moment(date).fromNow();
+  console.log(timeAgo);
   return (
-    <section>
-      <p>{comment_body}</p>
-      <p>{author}</p>
-      <p>{votes}</p>
-      <p>{comment_created_at}</p>
+    <section className="CommentCard">
+      <div className="profile_section">
+        <img
+          className="profile_img"
+          src="https://pngimg.com/uploads/circle/circle_PNG12.png"
+          alt="profile img"
+        />
+        <p className="profile_author">{author}</p>
+        <p className="comment_time">{timeAgo}</p>
+      </div>
+      <p className="comment_body">{comment_body}</p>
+      <div className="comment_votes">
+        <span className="votes">⬆️</span>
+        <p className="votes">{votes}</p>
+        <span className="votes">⬇️</span>
+      </div>
     </section>
   );
 };
