b97bab78d1b9b4b5ed893f951c779f103e733191
gldrobinson <gldrobinson@hotmail.co.uk>
2021-11-25 16:14:46 +0000

    refactors ReviewContext to UserContext and adds it in App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import "./App.css";
 import "./styling/review_home.css";
 import "./styling/category_home.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ReviewContext } from "./contexts/ReviewContext";
+import { UserContext } from "./contexts/UserContext";
 import Header from "./components/Header.js";
 import CategoryHome from "./components/category_home/CategoryHome";
 import { useState } from "react";
 import ReviewHome from "./components/review_home/ReviewHome";
 
 function App() {
-  const [review, setReview] = useState({});
+  const [user, setUser] = useState({ username: "jessjelly'" });
   return (
     <div className="App">
       <Header />
       <BrowserRouter>
-        <ReviewContext.Provider value={{ review, setReview }}>
+        <UserContext.Provider value={{ user, setUser }}>
           <Routes>
             <Route path="/" element={<CategoryHome />} />
             <Route path="/reviews" element={<CategoryHome />} />
@@ -24,7 +24,7 @@ function App() {
             />
             <Route path="/reviews/:review_id" element={<ReviewHome />} />
           </Routes>
-        </ReviewContext.Provider>
+        </UserContext.Provider>
       </BrowserRouter>
     </div>
   );
