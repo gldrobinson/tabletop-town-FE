62b5c3d2390addceb56c78d20b659ae761153a9b
gldrobinson <gldrobinson@hotmail.co.uk>
2021-11-26 17:55:30 +0000

    Starts adding username info in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react/cjs/react.development";
+import { UserContext } from "../contexts/UserContext";
 
 const Header = () => {
+  const { user } = useContext(UserContext);
   return (
     <header className="Header">
       <Link className="header_title" to="/">
         Tabletop Town
       </Link>
+      <h2>{user.username}</h2>
     </header>
   );
 };
---
+++ src/hooks/useUserInfo.js
@@ -0,0 +1,3 @@
+const useUserInfo = () => {};
+
+export default useUserInfo;
