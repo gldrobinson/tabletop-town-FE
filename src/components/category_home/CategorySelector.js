073198a826641c397d982fd6214a47b71a3ec7ff
gldrobinson <gldrobinson@hotmail.co.uk>
2021-11-26 14:43:59 +0000

    Removes duplicate loading gifs from category home

--- src/components/category_home/CategorySelector.js
+++ src/components/category_home/CategorySelector.js
@@ -1,14 +1,9 @@
 import { Link } from "react-router-dom";
-import Loading from "../reusabe_components/Loading";
-
 import useCategories from "../../hooks/useCategories";
 
 const CategorySelector = () => {
-  const { categories, isLoading, error } = useCategories();
+  const { categories, error } = useCategories();
 
-  if (isLoading) {
-    return <Loading />;
-  }
   if (error) return <p className="error_handling">{error}</p>;
 
   return (
