e3ef6fe7b75bbd5bb4561711567e338deb3088c6
gldrobinson <gldrobinson@hotmail.co.uk>
2021-11-29 09:58:01 +0000

    Starts on loading gif for when a user deletes a comment

--- src/components/reusable_components/DeleteButton.js
+++ src/components/reusable_components/DeleteButton.js
@@ -1,17 +1,31 @@
-import { useContext } from "react/cjs/react.development";
+import { useContext, useState } from "react/cjs/react.development";
 import { UserContext } from "../../contexts/UserContext";
 import { deleteComment } from "../../utils/api";
+import loading from "../../assets/loading.gif";
 
 const DeleteButton = ({ author, comment_id, setReviewsUpdated }) => {
   const { user } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleDelete = () => {
+    setIsLoading(true);
     deleteComment(comment_id).then(() => {
+      setIsLoading(false);
       setReviewsUpdated((currentVal) => {
         return currentVal + 1;
       });
     });
   };
 
+  if (isLoading)
+    return (
+      <img
+        className="loading_gif"
+        id="delete_loading"
+        src={loading}
+        alt="loading..."
+      />
+    );
   return (
     <main>
       {author === user.username ? (
